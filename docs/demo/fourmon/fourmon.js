e79eeb3005a8b7d77d82422e7545ed60544e6a27
Phil Norton <philipnorton42@gmail.com>
2025-05-04 23:22:35 +0100

    11: Work in progress on the fourmon game.

--- docs/demo/fourmon/fourmon.js
+++ docs/demo/fourmon/fourmon.js
@@ -3,43 +3,62 @@ let loose = false;
 
 let segments = [];
 segments[0] = [
-  {row: 0, column: 0,},
-  {row: 0, column: 1,},
-  {row: 1, column: 0,},
+  { row: 0, column: 0, },
+  { row: 0, column: 1, },
+  { row: 1, column: 0, },
 ];
 segments[1] = [
-  {row: 2, column: 0,},
-  {row: 3, column: 0,},
-  {row: 3, column: 1,},
+  { row: 2, column: 0, },
+  { row: 3, column: 0, },
+  { row: 3, column: 1, },
 ];
 segments[2] = [
-  {row: 0, column: 2,},
-  {row: 0, column: 3,},
-  {row: 1, column: 3,},
+  { row: 0, column: 2, },
+  { row: 0, column: 3, },
+  { row: 1, column: 3, },
 ];
 segments[3] = [
-  {row: 2, column: 3,},
-  {row: 3, column: 2,},
-  {row: 3, column: 3,},
+  { row: 2, column: 3, },
+  { row: 3, column: 2, },
+  { row: 3, column: 3, },
 ];
 segments[4] = [
-  {row: 1, column: 1,},
-  {row: 1, column: 2,},
-  {row: 2, column: 1,},
-  {row: 2, column: 2,},
+  { row: 1, column: 1, },
+  { row: 1, column: 2, },
+  { row: 2, column: 1, },
+  { row: 2, column: 2, },
 ];
 
 let segmentColours = [
-  'blue',
-  'yellow',
-  'red',
-  'green',
-  'purple',
+  {
+    active: 'blue',
+    inactive: 'navy',
+  },
+  {
+    active: 'yellow',
+    inactive: 'olive',
+  },
+  {
+    active: 'red',
+    inactive: 'maroon',
+  },
+  {
+    active: 'lime',
+    inactive: 'green',
+  },
+  {
+    active: 'fuchsia',
+    inactive: 'purple',
+  },
 ];
 
 let sequence = [];
-let position = 0;
+let position = 1;
 
+const defaultLightUpInterval = 30;
+let lightUpInterval = defaultLightUpInterval;
+
+let selectedSegment = undefined;
 
 //start sequence
 //increse sequence
@@ -48,35 +67,64 @@ let position = 0;
 //   segments 
 
 function createSequence() {
-
+  sequence = [];
+  for (let i = 0; i < 5; i += 1) {
+    sequence.push(Math.floor(Math.random() * 5));
+  }
 }
 
 function userActionClick(element) {
+  lightUpInterval = defaultLightUpInterval;
+
   // run through the sequence as the user clicks
-  let segment = '';
   for (let i = 0; i < segments.length; i += 1) {
     for (let j = 0; j < segments[i].length; j += 1) {
       if (segments[i][j].row === element.row && segments[i][j].column === element.column) {
-        segment = i;
+        selectedSegment = i;
         break;
       }
     }
   }
-  console.log(segment);
+
+  // colour = segmentColours[segment]['active']
+
+  // for (let j = 0; j < segments[segment].length; j += 1) {
+  //   let row = segments[segment][j].row;
+  //   let column = segments[segment][j].column;
+  //   fillBox(grid[row][column], segmentColours[segment]['active']);
+  // }
+
+  //console.log(segment);
 }
 
-function init() {
-  //createSequence();
+function drawSegments() {
   for (let i = 0; i < segments.length; i += 1) {
-    for (let j = 0; j < segments[i].length; j += 1) {
-      let row = segments[i][j].row;
-      let column = segments[i][j].column;
-      fillBox(grid[row][column], segmentColours[i]);
+    if (selectedSegment === i) {
+      colourSegment(segments[i], segmentColours[i]['active']);
+    } else {
+      colourSegment(segments[i], segmentColours[i]['inactive']);
     }
+    //    for (let j = 0; j < segments[i].length; j += 1) {
+    //let row = segments[i][j].row;
+    //let column = segments[i][j].column;
+    //if (selectedSegment === i) {
+    //  fillBox(grid[row][column], segmentColours[i]['active']);
+    //}
+    //else {
+    //  fillBox(grid[row][column], segmentColours[i]['inactive']);
+    //}
+    //  }
   }
 }
 
-function update() {
+
+function init() {
+  createSequence();
+  console.log(sequence);
+  drawSegments();
+}
+
+function update(delta) {
   if (loose === true) {
     // Crashed!
     // Flush the screen.
@@ -96,6 +144,58 @@ function update() {
 
 }
 
-function draw() {
-//  cls();
+function colourSegment(segment, colour) {
+  for (let i = 0; i < segment.length; i += 1) {
+    let row = segment[i].row;
+    let column = segment[i].column;
+    fillBox(grid[row][column], colour);
+  }
+}
+
+function playSequence() {
+  removeClickListener();
+
+  for (let sequencePosition = 0; sequencePosition < position; sequencePosition += 1) {
+
+    let segment = segments[sequence[sequencePosition]];
+
+    colourSegment(segment, segmentColours[sequence[sequencePosition]]['inactive']);
+    
+    // remove user interaction
+
+    
+    colourSegment(segment, segmentColours[sequence[sequencePosition]]['active']);
+
+    // pause the game
+    wait(100);
+
+    // for (let i = 0; i < segments.length; i += 1) {
+    //   for (let j = 0; j < segments[i].length; j += 1) {
+    //     let row = segments[i][j].row;
+    //     let column = segments[i][j].column;
+    //     if (sequence[sequencePosition] === i) {
+    //       fillBox(grid[row][column], segmentColours[i]['active']);
+    //     }
+    //     else {
+    //       fillBox(grid[row][column], segmentColours[i]['inactive']);
+    //     }
+    //   }
+    // }
+  }
+
+  addClickListener();
+}
+
+function draw(delta) {
+  drawSegments();
+
+  playSequence();
+
+
+  lightUpInterval -= (delta * 60);
+
+  if (lightUpInterval <= 0) {
+    selectedSegment = undefined
+    lightUpInterval = defaultLightUpInterval;
+  }
 }
